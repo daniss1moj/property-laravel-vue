4fc0e509b33795ec884f368296bacac47a83b43c
daniss1moj <danissimojblack@gmail.com>
2023-10-18 18:57:54 +0200

    catch exception

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -10,18 +10,24 @@ class OfferController extends Controller
 {
     public function store(Request $request, Listing $listing)
     {
-        $data = $request->validate([
-            'amount' => 'required|integer|min:1|max:200000000'
-        ]);
+        try {
+            $data = $request->validate([
+                'amount' => 'required|integer|min:1|max:200000000'
+            ]);
 
 
-        $offer = $request->user()->offers()->make($data);
-        $offer->listing()->associate($listing);
-        $offer->save();
+            $offer = $request->user()->offers()->make($data);
+            $offer->listing()->associate($listing);
+            $offer->save();
 
-        $listing->owner->notify(
-            new OfferMade($offer)
-        );
-        return redirect()->back()->with('success', 'Offer was made!');
+            $listing->owner->notify(
+                new OfferMade($offer)
+            );
+            return redirect()->back()->with('success', 'Offer was made!');
+
+        } catch (\Exception $exception) {
+            dd($exception);
+        }
+
     }
 }
